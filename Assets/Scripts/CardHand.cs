212c1c9539f06e60bda1fdc8ad5165e162e2a6d6
Sergey Agafonov <serj13serj@yandex.ru>
2024-03-02 21:54:23 +0400

    add headers, add number of cards to start rotate

--- Assets/Scripts/CardHand.cs
+++ Assets/Scripts/CardHand.cs
@@ -1,18 +1,23 @@
+using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class CardHand : MonoBehaviour
 {
+    [Header("Dependencies")]
     [SerializeField] private GameObject _cardPrefab;
+
     [SerializeField] private Transform _cardsContainer;
 
+    [Header("Settings")]
     [SerializeField] private float _cardsOffsetX;
 
-    [FormerlySerializedAs("_cardsRotationOffset")] [SerializeField]
-    private float _cardsOffsetRotationAngle;
-
+    [SerializeField] private int _numberOfCardsToStartRotate;
+    [SerializeField] private float _cardsOffsetRotationAngle;
     [SerializeField] private float _edgeCardMaxRotationAngle;
 
+    [SerializeField] private float _cardsLoweringOffsetBasedOnAngle;
+
+    [Header("Debug")]
     [SerializeField] private int _numberOfCards;
 
     private GameObject[] _cards;
@@ -24,8 +29,8 @@ public class CardHand : MonoBehaviour
         for (var i = 0; i < _numberOfCards; i++)
         {
             var card = Instantiate(_cardPrefab, _cardsContainer);
-            var cardPosition = GetCardPositionAndRotation(i, _numberOfCards, _cardsOffsetX, _cardsOffsetRotationAngle,
-                _edgeCardMaxRotationAngle);
+            var cardPosition = GetCardPositionAndRotation(i, _numberOfCards, _cardsOffsetX,
+                _cardsLoweringOffsetBasedOnAngle, _cardsOffsetRotationAngle, _edgeCardMaxRotationAngle, _numberOfCardsToStartRotate);
             card.transform.localPosition = cardPosition;
             card.transform.localRotation = Quaternion.Euler(0f, 0f, cardPosition.z);
 
@@ -38,18 +43,17 @@ public class CardHand : MonoBehaviour
         for (var i = 0; i < _cards.Length; i++)
         {
             var card = _cards[i];
-            var cardPosition = GetCardPositionAndRotation(i, _numberOfCards, _cardsOffsetX, _cardsOffsetRotationAngle,
-                _edgeCardMaxRotationAngle);
+            var cardPosition = GetCardPositionAndRotation(i, _numberOfCards, _cardsOffsetX,
+                _cardsLoweringOffsetBasedOnAngle, _cardsOffsetRotationAngle, _edgeCardMaxRotationAngle, _numberOfCardsToStartRotate);
             card.transform.localPosition = cardPosition;
             card.transform.localRotation = Quaternion.Euler(0f, 0f, cardPosition.z);
         }
     }
 
-    private static Vector3 GetCardPositionAndRotation(int handCardIndex, int cardsInHand, float cardsOffsetX,
-        float cardsOffsetRotationAngle, float edgeCardMaxRotationAngle)
+    private static Vector3 GetCardPositionAndRotation(int handCardIndex, int numberOfCardsInHand, float cardsOffsetX,
+        float cardsOffsetY, float cardsOffsetRotationAngle, float edgeCardMaxRotationAngle, int numberOfCardsToStartRotate)
     {
-        var centerIndex = cardsInHand / 2f - 0.5f;
-
+        var centerIndex = numberOfCardsInHand / 2f - 0.5f;
         var offsetFromCenter = handCardIndex - centerIndex;
 
         var positionX = offsetFromCenter * cardsOffsetX;
@@ -57,11 +61,18 @@ public class CardHand : MonoBehaviour
         var edgeCardRotationAngle = centerIndex * cardsOffsetRotationAngle;
         if (edgeCardRotationAngle > edgeCardMaxRotationAngle)
         {
-            cardsOffsetRotationAngle = edgeCardMaxRotationAngle / (cardsInHand / 2f);
+            cardsOffsetRotationAngle = edgeCardMaxRotationAngle / (numberOfCardsInHand / 2f);
         }
 
-        var rotationAngle = offsetFromCenter * -cardsOffsetRotationAngle;
+        var positionY = 0f;
+        var rotationAngle = 0f;
 
-        return new Vector3(positionX, 0f, rotationAngle);
+        if (numberOfCardsInHand > numberOfCardsToStartRotate)
+        {
+            positionY = Math.Abs(offsetFromCenter) * -cardsOffsetY;
+            rotationAngle = offsetFromCenter * -cardsOffsetRotationAngle;
+        }
+
+        return new Vector3(positionX, positionY, rotationAngle);
     }
 }
