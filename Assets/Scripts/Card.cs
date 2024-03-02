3adb2dc1f7fce868761702a918dc3ee0ed81eb34
Sergey Agafonov <serj13serj@yandex.ru>
2024-03-02 22:43:32 +0400

    change sibling index on hover

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -6,10 +6,12 @@ public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     [SerializeField] private float _scaleOnHover;
 
     private Vector3 _initialScale;
+    private int _initialSiblingIndex;
 
     private void Awake()
     {
         _initialScale = transform.localScale;
+        _initialSiblingIndex = transform.GetSiblingIndex();
     }
 
     public void SetActive(bool value)
@@ -26,10 +28,12 @@ public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     public void OnPointerEnter(PointerEventData eventData)
     {
         transform.localScale = _initialScale * _scaleOnHover;
+        transform.SetAsLastSibling();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         transform.localScale = _initialScale;
+        transform.SetSiblingIndex(_initialSiblingIndex);
     }
 }
