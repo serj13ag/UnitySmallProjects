140ec3fdbb6fc039d2ac0675cd775919b3f91a23
Sergey Agafonov <serj13serj@yandex.ru>
2024-03-23 20:53:18 +0400

    add springy slider handle scaling on drag

--- Assets/Scripts/Springs/SpringySlider.cs
+++ Assets/Scripts/Springs/SpringySlider.cs
@@ -10,7 +10,27 @@ namespace Springs
         [SerializeField] private RectTransform _handleSlideArea;
         [SerializeField] private RectTransform _handle;
 
+        [SerializeField] private float _handleScaleDelta;
+        [SerializeField] private float _handleSpringFrequency;
+        [SerializeField] private float _handleSpringDamping;
+
         private bool _isDragging;
+
+        private SpringyMotionParams _handleSpringyMotionParams;
+        private float _handleTargetDeltaScale;
+        private float _handleCurrentVelocity;
+        private float _handleDeltaScale;
+        private float _handleInitialScaleX;
+        private float _handleInitialScaleY;
+
+        private void Awake()
+        {
+            _handleSpringyMotionParams = new SpringyMotionParams();
+
+            var handleLocalScale = _handle.localScale;
+            _handleInitialScaleX = handleLocalScale.x;
+            _handleInitialScaleY = handleLocalScale.y;
+        }
 
         private void Update()
         {
@@ -27,16 +47,28 @@ namespace Springs
 
                 _handle.localPosition = new Vector3(handlePositionX, 0f, 0f);
             }
+
+            HandleSpringyScale();
         }
 
         public void OnPointerDown(PointerEventData eventData)
         {
             _isDragging = true;
+            _handleTargetDeltaScale = _handleScaleDelta;
         }
 
         public void OnPointerUp(PointerEventData eventData)
         {
             _isDragging = false;
+            _handleTargetDeltaScale = 0f;
+        }
+
+        private void HandleSpringyScale()
+        {
+            SpringyUtils.CalcDampedSpringMotionParams(ref _handleSpringyMotionParams, Time.deltaTime, _handleSpringFrequency, _handleSpringDamping);
+            SpringyUtils.UpdateDampedSpringMotion(ref _handleDeltaScale, ref _handleCurrentVelocity, _handleTargetDeltaScale, _handleSpringyMotionParams);
+
+            _handle.localScale = new Vector3(_handleInitialScaleX + _handleDeltaScale, _handleInitialScaleY + _handleDeltaScale, 1f);
         }
     }
 }
