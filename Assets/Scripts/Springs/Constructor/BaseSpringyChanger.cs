1ad00497d702d7d64d3c4148bbcfb18c4db8bd4c
Sergey Agafonov <serj13serj@yandex.ru>
2024-04-04 17:56:06 +0400

    add mirror from center to base springy changer

--- Assets/Scripts/Springs/Constructor/BaseSpringyChanger.cs
+++ Assets/Scripts/Springs/Constructor/BaseSpringyChanger.cs
@@ -5,20 +5,36 @@ namespace Springs.Constructor
     [RequireComponent(typeof(SpringySpring))]
     public abstract class BaseSpringyChanger : MonoBehaviour
     {
+        [SerializeField] private bool _mirrorFromCenter;
+
         private SpringySpring _springySpring;
 
         private void Awake()
         {
             _springySpring = GetComponent<SpringySpring>();
 
-            UpdateSpringParameter(_springySpring.CurrentSpringValue);
+            UpdateSpringParameter(GetCurrentSpringValue());
         }
 
         private void Update()
         {
-            UpdateSpringParameter(_springySpring.CurrentSpringValue);
+            UpdateSpringParameter(GetCurrentSpringValue());
         }
 
         protected abstract void UpdateSpringParameter(float currentSpringValue);
+
+        private float GetCurrentSpringValue()
+        {
+            var currentSpringValue = _springySpring.CurrentSpringValue;
+
+            if (_mirrorFromCenter)
+            {
+                currentSpringValue = currentSpringValue < 0.5f
+                    ? Mathf.InverseLerp(0f, 0.5f, currentSpringValue)
+                    : 1f - Mathf.InverseLerp(0.5f, 1f, currentSpringValue);
+            }
+
+            return currentSpringValue;
+        }
     }
 }
